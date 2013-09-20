34bf8cbd2bf2fe9c7dd57f34123957a2a4538af8
grahammiln <graham.miln@miln.eu>
2013-09-20 16:11:22 +0200

    Limiting input to doxygen to avoid encoding problems
    Some xml output from doxygen is incorrectly encoded. This causes sphinx
    to exit due to an expat error.
    
    xmllint highlights the problem as encoding issues. These need to be
    fixed but for now can be worked around by restricting doxygen input to
    just *.h files.

--- framework/Code/DKDrawingView.h
+++ framework/Code/DKDrawingView.h
@@ -22,7 +22,22 @@ typedef enum
 }
 DKCropMarkKind;
 
+/** @brief DKDrawingView is the visible "front end" for the DKDrawing architecture.
 
+ A drawing can have multiple views into the same drawing data model, each with independent scales, scroll positions and so forth, but
+ all showing the same drawing. Manipulating the drawing through any view updates all of the views. In many cases there will only be
+ one view. The views are not required to be in the same window.
+
+ The actual contents of the drawing are all supplied by DKDrawing - all this does is call it to render its contents.
+
+ If the drawing system is built by hand, the drawing owns the view controller(s), and some other object (a document for example) will own the
+ drawing. However, like NSTextView, if you don't build a system by hand, this creates a default one for you which it takes ownership
+ of. By default this consists of 3 layers - a grid layer, a guide layer and a standard object layer. You can change this however you like, it's
+ there just as a construction convenience.
+
+ Note that because the controllers are owned by the drawing, there is no retain cycle even when the view owns the drawing. Views are owned by
+ their parent view or window, not by their controller.
+*/
 @interface DKDrawingView : GCZoomView
 {
 @private
@@ -141,22 +156,3 @@ extern NSString* kDKDrawingViewVerticalTopMarkerName;
 extern NSString* kDKDrawingViewVerticalCentreMarkerName;
 extern NSString* kDKDrawingViewVerticalBottomMarkerName;
 
-/*
-
-DKDrawingView is the visible "front end" for the DKDrawing architecture.
-
-A drawing can have multiple views into the same drawing data model, each with independent scales, scroll positions and so forth, but
-all showing the same drawing. Manipulating the drawing through any view updates all of the views. In many cases there will only be
-one view. The views are not required to be in the same window.
-
-The actual contents of the drawing are all supplied by DKDrawing - all this does is call it to render its contents.
-
-If the drawing system is built by hand, the drawing owns the view controller(s), and some other object (a document for example) will own the
-drawing. However, like NSTextView, if you don't build a system by hand, this creates a default one for you which it takes ownership
-of. By default this consists of 3 layers - a grid layer, a guide layer and a standard object layer. You can change this however you like, it's
-there just as a construction convenience.
-
-Note that because the controllers are owned by the drawing, there is no retain cycle even when the view owns the drawing. Views are owned by
-their parent view or window, not by their controller.
-
-*/
