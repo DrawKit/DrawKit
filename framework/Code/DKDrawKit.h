cd5bbac2524bfe2d9d00f359ff41cca2299b6e0d
grahammiln <graham.miln@miln.eu>
2014-05-09 15:36:11 +0200

    Adding LGPL text as separate file
    Tidying up licence file locations within Xcode project.

--- framework/Code/DKDrawKit.h
+++ framework/Code/DKDrawKit.h
@@ -4,6 +4,7 @@
  @author Contributions from the community
  @date 2005-2014
  @copyright This software is released subject to licensing conditions as detailed in DRAWKIT-LICENSING.TXT, which must accompany this source file.
+ @licence LGPL3
  */
 
 #import "DKDrawingView.h"
